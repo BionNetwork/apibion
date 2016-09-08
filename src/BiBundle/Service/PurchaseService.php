b9fec861c07ca691a9211f4ebdc12c64f8f46b38
Tagir Zinnurov <zinnurov@etton.ru>
2016-09-08 22:46:28 +0300

    regular

--- src/BiBundle/Service/PurchaseService.php
+++ src/BiBundle/Service/PurchaseService.php
@@ -29,6 +29,7 @@ class PurchaseService extends UserAwareService
         $items = $this->getUser()->getPurchase();
         foreach ($items as $purchase) {
             if($card->getId() === $purchase->getCard()->getId()) {
+                return $purchased;
                 $purchased = true;
             }
         }
