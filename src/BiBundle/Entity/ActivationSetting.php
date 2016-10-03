af0f18eb96473a97c677d05faa9de176edd46ec8
Mikhail Zhuravlev <zhuravlev@etton.ru>
2016-10-03 14:04:37 +0300

    added ActivationSetting

---
+++ src/BiBundle/Entity/ActivationSetting.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace BiBundle\Entity;
+
+/**
+ * ActivationSetting
+ */
+class ActivationSetting
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * @var \DateTime
+     */
+    private $deletedOn;
+
+    /**
+     * @var Activation
+     */
+    private $activation;
+
+    /**
+     * @return Activation
+     */
+    public function getActivation()
+    {
+        return $this->activation;
+    }
+
+    /**
+     * @param Activation $activation
+     */
+    public function setActivation($activation)
+    {
+        $this->activation = $activation;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set key
+     *
+     * @param string $key
+     *
+     * @return ActivationSetting
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    /**
+     * Get key
+     *
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return ActivationSetting
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set deletedOn
+     *
+     * @param \DateTime $deletedOn
+     *
+     * @return ActivationSetting
+     */
+    public function setDeletedOn($deletedOn)
+    {
+        $this->deletedOn = $deletedOn;
+
+        return $this;
+    }
+
+    /**
+     * Get deletedOn
+     *
+     * @return \DateTime
+     */
+    public function getDeletedOn()
+    {
+        return $this->deletedOn;
+    }
+}
+
---
+++ src/BiBundle/Repository/ActivationSettingRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BiBundle\Repository;
+
+/**
+ * ActivationSettingRepository
+ *
+ */
+class ActivationSettingRepository extends \Doctrine\ORM\EntityRepository
+{
+}
